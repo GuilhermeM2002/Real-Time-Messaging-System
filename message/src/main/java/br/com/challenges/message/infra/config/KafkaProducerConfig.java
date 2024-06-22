133e6cf1f19210639029b61a2a49291bdc355a52
guilhermemr <guilherme.infor2@gmail.com>
2024-06-22 15:01:18 -0300

    hotfix: add the correct port to boostrap address

--- message/src/main/java/br/com/challenges/message/infra/config/KafkaProducerConfig.java
+++ message/src/main/java/br/com/challenges/message/infra/config/KafkaProducerConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -12,8 +13,9 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 
+@Configuration
 public class KafkaProducerConfig {
-    @Value("${spring.kafka.bootstrap-servers:localhost:29092}")
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
     private String bootstrapAddress;
 
     @Bean
