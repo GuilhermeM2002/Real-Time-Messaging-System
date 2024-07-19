788247890f27e8b951a227ed874064ee1d6e1b9f
guilhermemr <guilherme.infor2@gmail.com>
2024-07-19 17:49:16 -0300

    add: change producer and consumer kafka for avro

--- message/src/main/java/br/com/challenges/message/infra/config/KafkaProducerConfig.java
+++ message/src/main/java/br/com/challenges/message/infra/config/KafkaProducerConfig.java
@@ -1,6 +1,8 @@
 package br.com.challenges.message.infra.config;
 
-import br.com.challenges.message.application.dto.MessageDto;
+import br.com.challenges.message.avro.MessageAvro;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import io.confluent.kafka.serializers.KafkaAvroSerializerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 
@@ -19,7 +20,7 @@ public class KafkaProducerConfig {
     private String bootstrapAddress;
 
     @Bean
-    public ProducerFactory<String, MessageDto> producerFactory(){
+    public ProducerFactory<String, MessageAvro> producerFactory(){
         var props = new HashMap<String, Object>();
         props.put(
                 ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
@@ -28,13 +29,16 @@ public class KafkaProducerConfig {
                 ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 StringSerializer.class);
         props.put(
+                KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                "http://localhost:8081");
+        props.put(
                 ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-                JsonSerializer.class);
+                KafkaAvroSerializer.class);
 
         return new DefaultKafkaProducerFactory<>(props);
     }
     @Bean
-    public KafkaTemplate<String, MessageDto> kafkaTemplate(){
+    public KafkaTemplate<String, MessageAvro> kafkaTemplate(){
         return new KafkaTemplate<>(producerFactory());
     }
 }
--- user/src/main/java/br/com/challenges/user/infra/config/KafkaConsumerConfig.java
+++ user/src/main/java/br/com/challenges/user/infra/config/KafkaConsumerConfig.java
@@ -1,23 +1,22 @@
 package br.com.challenges.user.infra.config;
 
+import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.converter.MessageConverter;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 
 @Configuration
-@EnableKafka
 public class KafkaConsumerConfig {
-    @Value("${spring.kafka.bootstrap-servers:localhost:29092}")
+    @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
     private String bootstrapAddress;
     @Bean
     public ConsumerFactory<String, String> consumerFactory(){
@@ -29,13 +28,17 @@ public class KafkaConsumerConfig {
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
                 StringDeserializer.class);
         props.put(
+                KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                "http://localhost:8081");
+        props.put(
+                KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG,
+                true);
+        props.put(
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
-                StringDeserializer.class);
+                KafkaAvroDeserializer.class);
+        props.put(
+                JsonDeserializer.TRUSTED_PACKAGES, "*");
         return new DefaultKafkaConsumerFactory<>(props);
-    }
-    @Bean
-    public MessageConverter messageConverter(){
-        return new StringJsonMessageConverter();
     }
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(){
