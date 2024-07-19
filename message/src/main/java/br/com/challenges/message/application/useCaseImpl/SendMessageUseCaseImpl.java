b1f3a1c382b064342cbec13ba98cd9218750b8e6
guilhermemr <guilherme.infor2@gmail.com>
2024-07-19 17:55:03 -0300

    add: change methods to send avro class

--- message/src/main/java/br/com/challenges/message/application/useCaseImpl/SendMessageUseCaseImpl.java
+++ message/src/main/java/br/com/challenges/message/application/useCaseImpl/SendMessageUseCaseImpl.java
@@ -1,6 +1,7 @@
 package br.com.challenges.message.application.useCaseImpl;
 
 import br.com.challenges.message.application.dto.MessageDto;
+import br.com.challenges.message.avro.MessageAvro;
 import br.com.challenges.message.core.domain.Message;
 import br.com.challenges.message.core.useCases.SendMassageUseCase;
 import br.com.challenges.message.adapters.repository.MessageRepository;
@@ -9,6 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+
 @Service
 public class SendMessageUseCaseImpl implements SendMassageUseCase {
     @Autowired
@@ -16,13 +20,21 @@ public class SendMessageUseCaseImpl implements SendMassageUseCase {
     @Autowired
     private ModelMapper mapper;
     @Autowired
-    private KafkaTemplate<String, MessageDto> kafkaTemplate;
+    private KafkaTemplate<String, MessageAvro> kafkaTemplate;
     @Override
     public MessageDto sendMessage(MessageDto dto) {
         var message = mapper.map(dto, Message.class);
         repository.save(message);
         var messageDto = mapper.map(message, MessageDto.class);
-        kafkaTemplate.send("message-sent", messageDto);
+
+        var messageAvro = new MessageAvro();
+        messageAvro.setContent(message.getContent());
+        messageAvro.setDate(message.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        messageAvro.setId(message.getId().toString());
+        messageAvro.setWhoSend(message.getWhoSend());
+        messageAvro.setWhoReceive(message.getWhoReceive());
+
+        kafkaTemplate.send("message-sent", messageAvro);
 
         return messageDto;
     }
--- message/src/main/java/br/com/challenges/message/core/domain/Message.java
+++ message/src/main/java/br/com/challenges/message/core/domain/Message.java
@@ -8,7 +8,6 @@ import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.OffsetDateTime;
 import java.util.Date;
 
 @NoArgsConstructor
