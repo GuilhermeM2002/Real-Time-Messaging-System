5c5795e31ffe3351c0e7b97d5718dbb456b754e2
GuilhermeM2002 <guilherme.infor2@gmail.com>
2024-12-22 20:28:11 -0300

    update: save message and send to kafka

--- message/src/main/java/br/com/challenges/message/controller/MessagePrivateController.java
+++ message/src/main/java/br/com/challenges/message/controller/MessagePrivateController.java
@@ -1,15 +1,42 @@
 package br.com.challenges.message.controller;
 
+import br.com.challenges.message.adapters.repository.MessagePrivateRepository;
 import br.com.challenges.message.application.dto.MessageDto;
+import br.com.challenges.message.avro.MessageAvro;
+import br.com.challenges.message.core.domain.MessagePrivate;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
 
-@MessageMapping("private-message")
-@SendTo("/topic/private-chat")
+import java.time.ZoneId;
+
+@Controller
 public class MessagePrivateController {
+    @Autowired
+    private MessagePrivateRepository messagePrivateRepository;
+    @Autowired
+    private KafkaTemplate<String, MessageAvro> kafkaTemplate;
+    @Autowired
+    private ModelMapper mapper;
+
+    @MessageMapping("private-message")
+    @SendTo("/topic/private-chat")
     public MessageDto sendPrivateMessage(@Payload MessageDto dto) {
+        var message = mapper.map(dto, MessagePrivate.class);
+
+        var messageAvro = new MessageAvro();
+        messageAvro.setContent(message.getContent());
+        messageAvro.setDate(message.getDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
+        messageAvro.setId(message.getId().toString());
+        messageAvro.setWhoSend(message.getWhoSend());
+        messageAvro.setWhoReceive(message.getWhoReceive());
+
+        kafkaTemplate.send("message-sent", messageAvro.getWhoSend().toString(), messageAvro);
+        messagePrivateRepository.save(message);
         return dto;
     }
 }
